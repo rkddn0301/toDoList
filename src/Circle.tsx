333b1f217af9b2ce02695aebc8f4793f0bcedc20
rkddn0301 <rkddn0301@naver.com>
2024-07-15 12:05:15 +0900

    3.4 State modified: Circle.tsx

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 interface ContainerProps {
@@ -25,6 +26,12 @@ const Container = styled.div<ContainerProps>`
 
 // `text` props와 같이 미리 초기값을 정해줄 수도 있다.
 function Circle({ bgcolor, bordercolor, text = "default text" }: CircleProps) {
+  // TypeScript에서의 useState는 여러 타입을 지정해 주고싶을 때 `useState<number|string>(0)`으로 할 수 있다.
+  const [counter, setCounter] = useState<number | string>(0);
+
+  setCounter(2);
+  setCounter("hello");
+
   return (
     <Wrapper>
       <Container bgcolorss={bgcolor} bordercolorss={bordercolor ?? bgcolor}>
