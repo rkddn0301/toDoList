2fb51e6d96b0abd5dd57af8882e7afbad2961633
rkddn0301 <rkddn0301@naver.com>
2024-07-15 18:35:51 +0900

    5.1 Styles 	modified:   src/App.tsx 	modified:   src/routes/Coins.tsx 	modified:   src/styled.d.ts 	modified:   src/theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,8 @@
 import { DefaultTheme } from "styled-components";
 
+// 컬러 출처 : https://flatuicolors.com/palette/gb
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#4cd137",
 };
