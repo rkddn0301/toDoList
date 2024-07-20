1de84ef59a925eb9e4197e100b4988d3bf1551c2
rkddn0301 <rkddn0301@naver.com>
2024-07-20 17:05:31 +0900

    5.12 Price Chart

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -236,11 +236,11 @@ function Coin() {
           </Tabs>
 
           <Switch>
-            <Route path={`/${coinId}/price`}>
+            <Route path={`/:coinId/price`}>
               <Price />
             </Route>
-            <Route path={`/${coinId}/chart`}>
-              <Chart />
+            <Route path={`/:coinId/chart`}>
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
