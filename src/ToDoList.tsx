b5d3632ad587215aaed61942ab93e793b8e5812b
rkddn0301 <rkddn0301@naver.com>
2024-07-24 11:19:05 +0900

    6.6 Forms

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
-function ToDoList() {
+import { useForm } from "react-hook-form";
+
+/* function ToDoList() {
   const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
@@ -10,12 +13,40 @@ function ToDoList() {
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
+    setToDoError("");
     console.log(toDo);
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={toDo} onChange={onChange} placeholder="Write a to do" />
+        <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
+      </form>
+    </div>
+  );
+} */
+
+function ToDoList() {
+  const { register, watch } = useForm();
+  console.log(watch());
+  /*
+   * onChange : 태그 안에서 바뀌는 동작
+   * onFocus : 태그 안을 클릭했을 때 깜빡임
+   * onBlur : onFocus 상태가 아닌 것
+   */
+  return (
+    <div>
+      <form>
+        <input {...register("email")} placeholder="Write a email" />
+        <input {...register("userName")} placeholder="Write a userName" />
+        <input {...register("name")} placeholder="Write a name" />
+        <input {...register("passWord")} placeholder="Write a passWord" />
+        <input {...register("passWord1")} placeholder="Write a passwWord1" />
+
         <button>Add</button>
       </form>
     </div>
