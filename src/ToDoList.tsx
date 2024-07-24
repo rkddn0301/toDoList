e4dc1d7a500cc7cf820c3bcb1446a99517bfee84
rkddn0301 <rkddn0301@naver.com>
2024-07-24 17:12:01 +0900

    6.7 Form Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -31,8 +31,11 @@ import { useForm } from "react-hook-form";
 } */
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
   /*
    * onChange : 태그 안에서 바뀌는 동작
    * onFocus : 태그 안을 클릭했을 때 깜빡임
@@ -40,12 +43,37 @@ function ToDoList() {
    */
   return (
     <div>
-      <form>
-        <input {...register("email")} placeholder="Write a email" />
-        <input {...register("userName")} placeholder="Write a userName" />
-        <input {...register("name")} placeholder="Write a name" />
-        <input {...register("passWord")} placeholder="Write a passWord" />
-        <input {...register("passWord1")} placeholder="Write a passwWord1" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        {/* register 안에 required를 넣는 이유는 보안상으로 안전하기 때문 */}
+        <input
+          {...register("email", { required: true })}
+          placeholder="Write a email"
+        />
+        <input
+          {...register("userName", { required: true, minLength: 10 })}
+          placeholder="Write a userName"
+        />
+        <input
+          {...register("name", { required: true })}
+          placeholder="Write a name"
+        />
+        <input
+          {...register("passWord", { required: true, minLength: 5 })}
+          placeholder="Write a passWord"
+        />
+        <input
+          {...register("passWord1", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short.",
+            },
+          })}
+          placeholder="Write a passwWord1"
+        />
 
         <button>Add</button>
       </form>
