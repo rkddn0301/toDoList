80d1570a8758e58e13b1d82493e63c9eeb342267
rkddn0301 <rkddn0301@naver.com>
2024-07-25 16:35:34 +0900

    6.17 Selectors part Two

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -9,12 +9,16 @@ import {
 } from "recoil";
 import { StringLiteral } from "typescript";
 import CreateToDo from "./components/CreateToDo";
-import { toDoSelector, toDoState } from "./atoms";
+import { categoryState, toDoSelector, toDoState } from "./atoms";
 import ToDo from "./components/ToDo";
 
 function ToDoList() {
-  const [toDo, doing, done] = useRecoilValue(toDoSelector);
-
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (e: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(e.currentTarget.value);
+  };
+  console.log(category);
   /*
    * onChange : 태그 안에서 바뀌는 동작
    * onFocus : 태그 안을 클릭했을 때 깜빡임
@@ -22,34 +26,78 @@ function ToDoList() {
    */
   return (
     <div>
-      <h1>To Dos</h1>
-      <hr />
-      <CreateToDo />
-      <h2>To Do</h2>
-      {/* {...toDo}는 props를 받는 자식 컴포넌트가 같은 interface 타입일 때 사용 가능 */}
-      <ul>
-        {toDo.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Doing</h2>
-
-      <ul>
-        {doing.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Done</h2>
-
-      <ul>
-        {done.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      <div>
+        <h1>To Dos</h1>
+        <hr />
+        {/* onInput : 변경을 통해 입력된 값을 실시간으로 처리해주는 메서드 */}
+        <select value={category} onInput={onInput}>
+          <option value="TO_DO">To Do</option>
+          <option value="DOING">Doing</option>
+          <option value="DONE">Done</option>
+        </select>
+        <CreateToDo />
+        <ul>
+          {toDos?.map((toDo) => (
+            <ToDo key={toDo.id} {...toDo} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default ToDoList;
+
+/* 아래 코드는 일반 useState로 진행 했을 때이다.
+interface IToDo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+  function ToDoList() {
+    const [toDo, setToDo] = useState("");
+  
+    const [toDos, setToDos] = useState<IToDo[]>([]);
+  
+    const changeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+      setToDo(e.currentTarget.value);
+    };
+  
+    const onValid = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      setToDos((oldToDos) => [
+        { text: toDo, id: Date.now(), category: "TO_DO" },
+        ...oldToDos,
+      ]);
+      console.log(toDo);
+    };
+  
+
+    return (
+      <div>
+        <h1>To Dos</h1>
+        <hr />
+        <form
+          style={{ display: "flex", flexDirection: "column" }}
+          onSubmit={onValid}
+        >
+
+          <input
+            value={toDo}
+            onChange={changeHandler}
+            placeholder="Write a toDO"
+          />
+          <button>Add</button>
+        </form>
+        <h2>To Do</h2>
+        
+        <ul>
+        {...toDo}는 props를 받는 자식 컴포넌트가 같은 interface 타입일 때 사용 가능
+          {toDos.map((toDo) => (
+            <ToDo key={toDo.id} {...toDo} />
+          ))}
+        </ul>
+      </div>
+    );
+  } */
--- src/atoms.ts
+++ src/atoms.ts
@@ -11,15 +11,17 @@ export const toDoState = atom<IToDo[]>({
   default: [],
 });
 
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
+
 export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState);
+    const category = get(categoryState);
     // filter : 기존 배열 형식 내부에서 원하는 조건으로 찾아와 배열로 다시 반환
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ];
+    return toDos.filter((toDo) => toDo.category === category);
   },
 });
