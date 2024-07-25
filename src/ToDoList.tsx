e99584ac438dc5909817f69088759090377b1604
rkddn0301 <rkddn0301@naver.com>
2024-07-25 17:27:09 +0900

    6.18 Enums

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -9,16 +9,17 @@ import {
 } from "recoil";
 import { StringLiteral } from "typescript";
 import CreateToDo from "./components/CreateToDo";
-import { categoryState, toDoSelector, toDoState } from "./atoms";
+import { Categories, categoryState, toDoSelector, toDoState } from "./atoms";
 import ToDo from "./components/ToDo";
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoSelector);
   const [category, setCategory] = useRecoilState(categoryState);
   const onInput = (e: React.FormEvent<HTMLSelectElement>) => {
-    setCategory(e.currentTarget.value);
+    // as any : 상황에 맞지는 않는데 그냥 쓴다는 의미.(신뢰성)
+    setCategory(e.currentTarget.value as any);
   };
-  console.log(category);
+  console.log(toDos);
   /*
    * onChange : 태그 안에서 바뀌는 동작
    * onFocus : 태그 안을 클릭했을 때 깜빡임
@@ -31,9 +32,9 @@ function ToDoList() {
         <hr />
         {/* onInput : 변경을 통해 입력된 값을 실시간으로 처리해주는 메서드 */}
         <select value={category} onInput={onInput}>
-          <option value="TO_DO">To Do</option>
-          <option value="DOING">Doing</option>
-          <option value="DONE">Done</option>
+          <option value={Categories.TO_DO}>To Do</option>
+          <option value={Categories.DOING}>Doing</option>
+          <option value={Categories.DONE}>Done</option>
         </select>
         <CreateToDo />
         <ul>
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,5 +1,5 @@
 import { useSetRecoilState } from "recoil";
-import { IToDo, toDoState } from "../atoms";
+import { Categories, IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
@@ -18,20 +18,22 @@ function ToDo({ text, category, id }: IToDo) {
       ];
     });
   };
+
+  console.log(category);
   return (
     // 보통 onClick에서 호출할 때 : onClick={onClick} 이런식으로 하지만
     // 인자를 보내야 할 경우 : onClick={() => onClick("인자")} 이렇게 익명의 함수를 선언해서 보내줘야 한다.
     // 이유는 onClick 기능 자체가 원래 함수를 호출하는 것이기 때문.
     <li>
       <span>{text}</span>
-      {category !== "DOING" && (
-        <button onClick={() => onClick("DOING")}>Doing</button>
+      {category !== Categories.DOING && (
+        <button onClick={() => onClick(Categories.DOING)}>Doing</button>
       )}
-      {category !== "TO_DO" && (
-        <button onClick={() => onClick("TO_DO")}>To Do</button>
+      {category !== Categories.TO_DO && (
+        <button onClick={() => onClick(Categories.TO_DO)}>To Do</button>
       )}
-      {category !== "DONE" && (
-        <button onClick={() => onClick("DONE")}>Done</button>
+      {category !== Categories.DONE && (
+        <button onClick={() => onClick(Categories.DONE)}>Done</button>
       )}
     </li>
   );
