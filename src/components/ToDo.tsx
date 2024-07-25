fdf1453f102e57618ebae839a30c81cd7763d11f
rkddn0301 <rkddn0301@naver.com>
2024-07-25 14:18:00 +0900

    6.14 Immutability part One

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -5,6 +5,15 @@ function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
   const onClick = (newCategory: IToDo["category"]) => {
     console.log("I wanna to ", newCategory);
+    setToDos((oldToDos) => {
+      // findIndex : 배열 내부에 index를 찾아주며, 아래 코드와 같이 조건부에 따라 특정 index를 가져올 수 있다.
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: newCategory };
+      console.log(oldToDo);
+      console.log(newToDo);
+      return oldToDos;
+    });
   };
   return (
     // 보통 onClick에서 호출할 때 : onClick={onClick} 이런식으로 하지만
